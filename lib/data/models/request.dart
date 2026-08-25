ee09a3ff1f350da6e75586b2196fbd1bbe843119
Abdelillah-CHADLI <chadlimohamedabdelillah@gmail.com>
2026-08-25 23:30:33 +0100

    add attendance status to request model

--- lib/data/models/request.dart
+++ lib/data/models/request.dart
@@ -1,10 +1,12 @@
 enum RequestStatus { pending, rejected, accepted }
+enum AttendanceStatus { absent, present }
 
 class Request {
   final int id;
   final int gameId;
   final int userId;
   final RequestStatus status;
+  final AttendanceStatus attendance;
   final DateTime createdAt;
   final DateTime updatedAt;
 
@@ -13,6 +15,7 @@ class Request {
     required this.gameId,
     required this.userId,
     required this.status,
+    required this.attendance,
     required this.createdAt,
     required this.updatedAt,
   });
@@ -23,6 +26,7 @@ class Request {
       gameId: json['game_id'],
       userId: json['user_id'],
       status: RequestStatus.values.byName(json['status']),
+      attendance: AttendanceStatus.values.byName(json['attendance']),
       createdAt: DateTime.parse(json['created_at']),
       updatedAt: DateTime.parse(json['updated_at']),
     );
@@ -34,6 +38,7 @@ class Request {
       'game_id': gameId,
       'user_id': userId,
       'status': status.name,
+      'attendance': attendance.name,
       'created_at': createdAt.toIso8601String(),
       'updated_at': updatedAt.toIso8601String(),
     };
