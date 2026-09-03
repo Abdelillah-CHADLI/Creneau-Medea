d41f1f85898f5b11cf1cc8aa8acd5b292a0dc029
Abdelillah-CHADLI <chadlimohamedabdelillah@gmail.com>
2026-09-03 21:37:41 +0100

    set android app display name, name and launcher icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,18 +5,19 @@ plugins {
 }
 
 android {
-    namespace = "com.example.creneau_medea"
+    namespace = "dz.creneaumedea.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.creneau_medea"
+        applicationId = "dz.creneaumedea.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -42,4 +43,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
